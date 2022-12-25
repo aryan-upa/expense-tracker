0738ee4c4d7d458668b21d990c2090d2cfdfca6c
Aryan Upadhyay <aryan.offc@gmail.com>
2022-12-26 00:42:39 +0530

    Some minor change.

--- src/components/Expenses/ExpensesFilter.jsx
+++ src/components/Expenses/ExpensesFilter.jsx
@@ -7,18 +7,26 @@ const ExpensesFilter = (props) => {
     props.onChangeFilter(event.target.value);
   };
 
+  const maxYr = new Date().getFullYear();
+  let yearList = new Array(maxYr - 2018);
+
+  for (let i = 0; i < yearList.length; i++) {
+    yearList[i] = (
+        <option key={2019 + i} value={"" + (2019 + i)}>
+          {2019 + i}
+        </option>
+    );
+  }
+
   return (
-    <div className="expenses-filter">
-      <div className="expenses-filter__control">
-        <label>Filter by year</label>
-        <select value={props.selected} onChange={dropdownChangeHandler}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
-        </select>
+      <div className="expenses-filter">
+        <div className="expenses-filter__control">
+          <label>Filter by year</label>
+          <select value={props.selected} onChange={dropdownChangeHandler}>
+            {yearList.map((val) => val)}
+          </select>
+        </div>
       </div>
-    </div>
   );
 };
 
